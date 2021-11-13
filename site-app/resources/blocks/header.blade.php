64adba58981bab8805c055b24dd6497760a44648
Ямшанов Константин Алексеевич <konstantin.yamshanov@bsc-ideas.com>
2021-11-13 12:18:44 +0400

    вынес блоки(хедер, футер)

---
+++ site-app/resources/blocks/header.blade.php
@@ -0,0 +1,25 @@
+<header class="p-3 text-white" >
+<div class="container">
+  <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+    <a href="/" class="navbar-brand">
+      <!-- Logo Image -->
+      <img src="images/igis_logo_lett.png" width="35" alt="" class="d-inline-block align-middle mr-2">
+    </a>
+
+    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+      <li><a href="/" class="nav-link px-2 text-secondary">Главная</a></li>
+      <li><a href="/doctors_main" class="nav-link px-2 text-white">Врачи</a></li>
+      <li><a href="/polyclinics_main" class="nav-link px-2 text-white">Поликлиники</a></li>
+    </ul>
+
+    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+      <input type="search" class="form-control form-control-dark" placeholder="Поиск..." aria-label="Search">
+    </form>
+
+    <div class="text-end">
+      <button type="button" onclick="location.href='/login'" class="btn btn-outline-light me-2">Войти</button>
+      <button type="button" onclick="location.href='/registration'" class="btn btn-warning">Регистрация</button>
+    </div>
+  </div>
+</div>
+</header>
