497b619182df56da0da7dc52509d2508bde9fc2c
ametr1ne <nikon.filanovsky@gmail.com>
2021-11-24 18:56:33 +0400

    проблемы с сеткой

--- public/js/anim.js
+++ public/js/anim.js
@@ -72,3 +72,32 @@ function deleteDoctor() {
     document.getElementById('doctor').value = ""
 }
 
+var elements = document.getElementsByClassName("list_item");
+var i;
+// Представление списка
+function listView() {
+  document.querySelector('.grid_view').style.background = 'rgba(183, 177, 177, 1)';
+  document.querySelector('.list_view').style.background = 'rgba(138, 174, 242, 1)';
+  for (i = 0; i < elements.length; i++) {
+      (document.getElementsByClassName('description'))[i].style.display = '';
+      (document.getElementsByClassName('address'))[i].style.display = '';
+      (document.getElementsByClassName('number'))[i].style.display = '';
+      elements[i].style.cssText = "background: ''; width: 100%; margin-bottom: 10px; cursor: pointer; padding: 10px; position: static;";
+  }
+}
+
+// Представление сетки
+function gridView() {
+  document.querySelector('.grid_view').style.background = 'rgba(138, 174, 242, 1) url("../storage/grid.png") center no-repeat';
+  document.querySelector('.list_view').style.background = 'rgba(183, 177, 177, 1)';
+
+  for (i = 0; i < elements.length; i++) {
+    (document.getElementsByClassName('description'))[i].style.display = 'none';
+    (document.getElementsByClassName('address'))[i].style.display = 'none';
+    (document.getElementsByClassName('number'))[i].style.display = 'none';
+    elements[i].style.cssText = "position: relative; margin-right: 40px; width: 25%; border: none; border-radius: 25px; background: #8AAEF2; width: 260px; height: 300px;";
+    (document.getElementsByClassName('photo'))[i].style.cssText = "top: 10%; width: 150px;";
+    (document.getElementsByClassName('text_about_title'))[i].style.cssText = "top: 50%; text-align: center;";
+  }
+}
+
