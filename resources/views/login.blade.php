03fe2c7a0cf1a54a743bd9e2f54ceccda64f3a13
ametr1ne <nikon.filanovsky@gmail.com>
2021-12-01 19:05:17 +0400

    валидация

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,9 +7,15 @@
     <div class="container a">
         <div class="login-box">
             <h1 class="main_title">Авторизация</h1>
-            <input type="text" class="input_login" placeholder="Введите вашу фамилию...">
-            <input type="text" class="input_login" placeholder="Введите ваш номер полиса...">
-            <button class="login-btn">Войти</button>
+            <form action="">
+                <div class="name-box">
+                    <input type="text" class="input_login" required placeholder="Введите вашу фамилию...">
+                    <p class="error"></p>
+                </div>
+                <input type="text" class="input_login" required placeholder="Введите ваш номер полиса...">
+                <p class="error"></p>
+                <button class="login-btn">Войти</button>
+            </form>
         </div>
     </div>
 @endsection
