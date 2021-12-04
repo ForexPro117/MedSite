7b8043be4fb361653856f6797177feaf777ca9d8
ForexPro <roma.minihanowa@mail.ru>
2021-12-04 22:37:16 +0400

    слияние мастера и ветки admin/feature

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/appointment', function () {
     return view('appointment');
 });
