d3551b77aeb7b39e8dcf3c581fd1908965b2bda9
ForexPro <roma.minihanowa@mail.ru>
2021-12-15 02:04:23 +0400

    переименование

--- resources/views/doctor-about.blade.php
+++ resources/views/doctor-about.blade.php
@@ -12,7 +12,7 @@
             <p class="experience">Стаж: <span>{{random_int(5,10)}} лет</span></p>
             <p class="title_desc">Описание: </p>
             <span class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consequatur cumque eligendi enim ex exercitationem, in iusto laborum minus neque officiis omnis placeat quasi quibusdam sit vel voluptas? Eveniet, sit?</span>
-            <p class="available_num_title">Доступные номерки: </p>
+            <p class="available_num_title">Расписание врача:</p>
             @for($i=0;$i<count($timetable);$i++)
             <div class="available_days">
                 <p class="day" onclick="showHide({{$i}})">{{now()->addHour(4)->addDay($i)->dayName}}, {{now()->addHour(4)->addDay($i)->format('d.m.Y')}}</p>
